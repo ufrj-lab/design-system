32868364a24324a484b7dae1265aafb617c7de14
Vinícius Carvalho <vinicius.fc93@gmail.com>
2019-02-02 02:21:59 +0000

    fix font

--- components/base/rollup.config.js
+++ components/base/rollup.config.js
@@ -1,13 +1,16 @@
 /* eslint-env node */
 import { genConf, genNames } from '../../helpers/genRollupConfig'
+import copy from 'rollup-plugin-copy'
 const { mnv, name } = require('./package.json')
 
 const { slug, script } = genNames(name)
 
-const { targets, clear } = mnv
+const { targets, clear, copy: cp } = mnv
 
-const conf = targets.map(target =>
-  genConf(target.type, slug, script, clear, target.injectCss)
-)
+const conf = targets.map(target => {
+  const result = genConf(target.type, slug, script, clear, target.injectCss)
+  result.plugins.push(copy({ ...cp }))
+  return result
+})
 
 export default conf
