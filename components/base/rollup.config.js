fbd56e121c96f6ea561e604c1d22770409c906bd
Vinícius Carvalho <vinicius.fc93@gmail.com>
2019-02-07 23:27:31 +0000

    fix rollup build

--- components/base/rollup.config.js
+++ components/base/rollup.config.js
@@ -1,26 +1,5 @@
 /* eslint-env node */
-import { genConf, genNames } from '../../helpers/genRollupConfig'
-import copy from 'rollup-plugin-copy'
+import { genConf } from '../../helpers/genRollupConfig'
 const { mnv, name, dependencies } = require('./package.json')
 
-const { slug, script } = genNames(name)
-
-const { targets, clear, copy: cp } = mnv
-
-const prod = !process.env.ROLLUP_WATCH
-
-const conf = targets.map(target => {
-  const result = genConf(
-    target.type,
-    slug,
-    script,
-    clear,
-    target.injectCss,
-    '/',
-    dependencies
-  )
-  result.plugins.push(prod && copy({ ...cp[target.type] }))
-  return result
-})
-
-export default conf
+export default genConf(name, mnv, dependencies, '/')
--- components/colors/rollup.config.js
+++ components/colors/rollup.config.js
@@ -1,21 +1,5 @@
 /* eslint-env node */
-import { genConf, genNames } from '../../helpers/genRollupConfig'
+import { genConf } from '../../helpers/genRollupConfig'
 const { mnv, name, dependencies } = require('./package.json')
 
-const { slug, script } = genNames(name)
-
-const { targets, clear } = mnv
-
-const conf = targets.map(target =>
-  genConf(
-    target.type,
-    slug,
-    script,
-    clear,
-    target.injectCss,
-    __dirname,
-    dependencies
-  )
-)
-
-export default conf
+export default genConf(name, mnv, dependencies, __dirname)
